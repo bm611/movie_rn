29e1dff327107be5ec8b72cbbb53a1de3ac6929b
bm611 <bharath.mohan.pro@gmail.com>
2025-04-19 16:43:12 -0700

    Create tab navigation layout and screens
    The commit implements a tab-based navigation structure with Home, Search,
    Profile and Saved screens, plus a route for individual movie details.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-center text-4xl text-primary font-bold">Welcome to TMDB!</Text>
+    </View>
+  );
+};
+
+export default Index;
