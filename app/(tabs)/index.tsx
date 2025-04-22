adc02f720583a01aedef2204a6be726fede0fbd5
bm611 <bharath.mohan.pro@gmail.com>
2025-04-21 22:25:39 -0700

    search page init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView, StatusBar } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 const Index = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-center text-4xl text-primary font-bold">
-        Welcome to TMDB!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <StatusBar
+        backgroundColor="transparent" // Makes the status bar background transparent
+        translucent={true} // Allows content to render behind the status bar
+        barStyle="light-content" // Makes the status bar items (time, battery, etc.) white
+      />
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image
+          source={icons.go_pop}
+          className="w-40 h-20 mt-20 mb-5 mx-auto"
+          resizeMode="contain"
+        />
+        <SearchBar
+          onPress={() => router.push("/search")}
+          placeholder="Search for a movie"
+        />
+      </ScrollView>
     </View>
   );
 };
